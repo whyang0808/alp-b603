4e2cc34c94228fd761de89b863f9cc27f0955e62
Shawn <shawn2798@icloud.com>
2021-06-26 18:38:36 +0800

    show docs in browser

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import swaggerUi from "swagger-ui-express";
 import userRoutes from './routes/user'
 import authRoutes from './routes/auth'
-import documentationRoute from './routes/documentation'
+import apiDocs from "./documentation/apidocs.json";
 
 // Setup env
 dotenv.config()
@@ -37,6 +38,6 @@ app.use(cookieParser())
 // Configure routes
 app.use('/user', userRoutes)
 app.use('/auth', authRoutes)
-app.use('/docs', documentationRoute)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`))
